a2fdfb999cf1b1435457e7346210a173a2497e74
Yosuke Kumakura <kumatch@gmail.com>
2018-11-28 12:54:29 +0900

    9章終了 (Implement advanced login)

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,14 +6,38 @@ module SessionsHelper
 
   # 現在のユーザーをログアウトする
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
-  end  
+  end
 
-  # 現在のログイン中ユーザ情報を返す。非ログインならば nil を返す。
+  # ユーザーのセッションを永続的にする
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  # 永続的セッションを破棄する
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
+  # 現在のログイン中ユーザ情報を返す
+  #  1. セッション情報があればそれを元に処理する
+  #  2. 記憶情報があればそれを元に参照し、かつログイン状態にする
+  #  3. 非ログインならば nil を返す
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
 
